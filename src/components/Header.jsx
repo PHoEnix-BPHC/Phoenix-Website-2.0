ae5d6d745c2340b158a60bfe69a785e0682847f1
Arnab <arnangoswamiam@gmail.com>
2023-11-11 15:16:01 +0530

    style the active page differently in the navbar (header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,33 @@
 import logoSvg from '../assets/phoenix-logo-cropped.svg';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-		<header className="font-grotesk grid grid-rows-[15vh] grid-cols-2 p-[3vh]">
-			<img src={logoSvg} alt="Phoenix Logo" className="w-[12%]" />
-			<nav className="flex justify-end">
-				<Link to='/' className="p-[3%]">Home</Link>
-				<Link to='/projects' className="p-[3%]">Projects</Link>
-				<Link to='/professors' className="p-[3%]">Professors</Link>
-				<Link to='/pors' className="p-[3%]">PORs</Link>
-				<Link to='/it-team' className="p-[3%]">IT Team</Link>
-			</nav>
-		</header>
-    )
+	const navigationOptions = [
+		{ label: 'Home', route: '/'},
+		{ label: 'Projects', route: '/projects'},
+		{ label: 'Professors', route: '/professors'},
+		{ label: 'PORs', route: '/pors'},
+		{ label: 'IT Team', route: '/it-team'},
+	]
+
+	return (
+	<header className="font-grotesk grid grid-rows-[15vh] grid-cols-2 p-[3vh]">
+		<img src={logoSvg} alt="Phoenix Logo" className="w-[12%]" />
+		<nav className="flex justify-end">
+		{
+		navigationOptions.map( (option, index) => (
+			<NavLink to={option.route} 
+			key={`nav option ${index}`}
+			className={ ({isActive}) => {
+				return `p-[3%] ${isActive ? 'bg-gradient-to-r from-red to-yellow text-transparent bg-clip-text' : ''}`
+			} }>
+				{option.label}
+			</NavLink>
+		) )
+		}
+		</nav>
+	</header>
+	)
 }
 
 export default Header;
