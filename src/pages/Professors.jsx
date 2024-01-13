08efd4dd39ef7256f53b5b077d34467bf35372b5
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-01-13 23:24:21 +0530

    professor page

--- src/pages/Professors.jsx
+++ src/pages/Professors.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import professors from '../data/professors.js';
+import { Accordion, Figure } from 'react-bootstrap';
 
 const Professors = () => {
 
@@ -11,22 +12,48 @@ const Professors = () => {
 		let vals = Object.values(obj);
 		let lijsx = [];
 
-		for (let i = 1; i < keys.length; ++i) {
+		for (let i = 2; i < keys.length; ++i) {
 			if (keys[i] === currentKey && highlightLength > 0) {
-				lijsx.push(<li key={i}><span className="font-bold text-[1.5vw]">{keys[i]}</span><span className="text-[1.2vw]"> : {vals[i].slice(0, highlightIndex)}</span><span className="text-[1.2vw] bg-yellow">{vals[i].slice(highlightIndex, highlightIndex + highlightLength)}</span><span className="text-[1.2vw]">{vals[i].slice(highlightIndex + highlightLength)}</span></li>);
+				lijsx.push(<li key={i}>
+						<span className="font-bold text-[1.5vw]">{keys[i]}</span>
+						<span className="text-[1.2vw]"> : {vals[i].slice(0, highlightIndex)}</span>
+						<span className="text-[1.2vw] bg-yellow">{vals[i].slice(highlightIndex, highlightIndex + highlightLength)}</span>
+						<span className="text-[1.2vw]">{vals[i].slice(highlightIndex + highlightLength)}</span>
+					</li>);
 			} else {
-				lijsx.push(<li key={i}><span className="font-bold text-[1.5vw]">{keys[i]}</span><span className="text-[1.2vw]"> : {vals[i]}</span></li>);
+				lijsx.push(<li key={i}>
+						<span className="font-bold text-[1.5vw]">{keys[i]}</span>
+						<span className="text-[1.2vw]"> : {vals[i]}</span>
+					</li>);
 			}
 		}
 
 		return lijsx;
 	}
 
-	let allProfDataJsx = professors.map((professor) => 
-		<ul>
-			<li key={professor.id} className="my-[10vh]"><img src={"../data/professorImages/" + professor.faculty + ".png"}/><ul>{liForEachProfessorData(professor)}</ul></li>
-		</ul>
-	);
+	let allProfDataJsx = professors.map((professor) => {
+		let src = "/src/data/professorImages/" + professor.faculty + ".png";
+
+		return (<Accordion className="m-[5%]">
+			<Accordion.Item eventKey={professor.id}>
+				<Accordion.Header>
+					<Figure>
+						<Figure.Image width={150} height={150} src={src} />
+						<Figure.Caption><p className="text-lg">{professor.faculty}</p></Figure.Caption>
+					</Figure>
+				</Accordion.Header>
+				<Accordion.Body>
+					<ul>{liForEachProfessorData(professor)}</ul>
+				</Accordion.Body>
+			</Accordion.Item>
+		</Accordion>)
+	});
+
+	allProfDataJsx = (
+		<div className="grid grid-cols-2 items-center">
+			{allProfDataJsx}
+		</div>
+	)
 
 	const [profDataJsx, setProfDataJsx] = useState(allProfDataJsx);
 
@@ -37,10 +64,22 @@ const Professors = () => {
 		let newProfDataJsx = professors.map((professor) => {
 			let lowerKey = professor[currentKey].toLowerCase();
 			let lowerSearch = search.toLowerCase();
+			let src = "/src/data/professorImages/" + professor.faculty + ".png";
+			
 			if (lowerKey.includes(lowerSearch)) {
-				return (<ul>
-					<li key={professor.id} className="my-[10vh]"><ul>{liForEachProfessorData(professor, lowerKey.indexOf(lowerSearch), lowerSearch.length)}</ul></li>
-				</ul>);
+				return (<Accordion>
+						<Accordion.Item eventKey={professor.id}>
+							<Accordion.Header>
+								<Figure>
+									<Figure.Image width={150} height={150} src={src} />
+									<Figure.Caption><p className="text-lg">{professor.faculty}</p></Figure.Caption>
+								</Figure>
+							</Accordion.Header>
+							<Accordion.Body>
+								<ul>{liForEachProfessorData(professor, lowerKey.indexOf(lowerSearch), lowerSearch.length)}</ul>
+							</Accordion.Body>
+						</Accordion.Item>
+					</Accordion>);
 			};
 		});
 		
@@ -71,16 +110,18 @@ const Professors = () => {
 
 	return (
 		<div>
-			<div className="m-[5vw]">
-				<input className="box-border w-[60vw] h-[7vh] p-[1vw] rounded bg-lightestgray focus:bg-lightgray outline-none" type="text" placeholder="Search..." onChange={newSearch}></input>
-				<span className="group">
-					<button className="relative h-[7vh] w-[10vw] top-[0.2vh] rounded text-white bg-red hover:bg-lightgray text-[1.5vw]">{currentKey}</button>
-					<div className="hidden group-hover:inline-block absolute">
-						<ul>{dropdownOptions}</ul>
-					</div>
-				</span>
+			<div className="m-[5vw] flex flex-col items-center">
+				<div>
+					<input className="box-border w-[60vw] h-[7vh] p-[1vw] rounded bg-lightestgray focus:bg-lightgray outline-none" type="text" placeholder="Search..." onChange={newSearch}></input>
+					<span className="group">
+						<button className="relative h-[7vh] w-[10vw] top-[0.2vh] rounded text-white bg-red hover:bg-lightgray text-[1.5vw]">{currentKey}</button>
+						<div className="hidden group-hover:inline-block absolute">
+							<ul>{dropdownOptions}</ul>
+						</div>
+					</span>
+				</div>
 			</div>
-			<h1 className="text-[5vw] m-[1vw]">Professors</h1>
+			<h1 className="text-[6vw] m-[1vw] text-center bg-gradient-to-r from-red to-yellow text-transparent bg-clip-text">Professors</h1>
 
 			<ul className="m-[2vw]">{profDataJsx}</ul>
 		</div>
