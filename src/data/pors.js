0e6f0aa732107dca567f50526556cae66855ef34
satansin123 <sanchaynts9422@gmail.com>
2024-01-13 12:10:33 +0530

    pors page is more or less done

---
+++ src/data/pors.js
@@ -0,0 +1,60 @@
+const pors = [
+    {
+        id: 1,
+        student: "Ujjwal Kharangar" ,
+        designation: "President",
+        contact: 6397442450
+    },
+
+    {
+        id: 2,
+        student: "R.Hariharan" ,
+        designation: "Secretary",
+        contact: 6397442450
+    },
+
+    {
+        id: 3,
+        student: "Shubhanga Gautam" ,
+        designation: "Treasurer",
+        contact: 6397442450
+    },
+
+    {
+        id: 4,
+        student: "Saketh VK" ,
+        designation: "Technical Head",
+        contact: 6397442450
+    },
+
+    {
+        id: 5,
+        student: "Nishitha Deverakonda" ,
+        designation: "Design Head",
+        contact: 6397442450
+    },
+
+    {
+        id: 6,
+        student: "Srivenkat Varma Nandimandalam" ,
+        designation: "Editorial Head",
+        contact: 6397442450
+    },
+
+    {
+        id: 7,
+        student: "Sanchay Ketan Sinha" ,
+        designation: "Joint Secretary",
+        contact: 6397442450
+    },
+
+    {
+        id: 8,
+        student: "Vedant Dutta" ,
+        designation: "Joint Treasurer",
+        contact: 6397442450
+    }
+
+]
+
+export default pors;
--- src/pages/Pors.jsx
+++ src/pages/Pors.jsx
@@ -1,7 +1,34 @@
+import React from 'react';
+import pors from '../data/pors';
+
+const Card = ({ id, student, designation, contact }) => {
+  
+  const imagePath = `src/data/porImages/${id}.jpeg`; 
+
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl overflow-hidden shadow-md">
+      <div className="md:flex">
+        <div className="md:flex-shrink-0">
+          <img className="h-48 w-full object-cover md:w-48" src={imagePath} alt={`${student}`} />
+        </div>
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{designation}</div>
+          <p className="mt-2 text-gray-500">{student}</p>
+          <p className="mt-2 text-gray-500">{contact}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Pors = () => {
   return (
-    <div>PORS Page</div>
-  )
-}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-8">
+      {pors.map((por) => (
+        <Card key={por.id} {...por} />
+      ))}
+    </div>
+  );
+};
 
 export default Pors;
